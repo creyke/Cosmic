1a8ac5a33a0cc2dc2cde996fcb37ce10ae175f3a
Roger Creyke <roger.creyke@odds-bods.com>
2019-10-21 21:38:48 +0100

    Changed parameter names.

--- Cosmic/Commands/QueryBaseOptions.cs
+++ Cosmic/Commands/QueryBaseOptions.cs
@@ -10,31 +10,31 @@ namespace Cosmic.Commands
         [Value(1, Required = true, HelpText = "Query e.g. \"SELECT * FROM c\"")]
         public string Query { get; set; }
 
-        [Value(2, HelpText = "Default argument e.g 'id'.")]
+        [Value(2, HelpText = "Query parameter e.g 'id'.")]
         public string Value1 { get; set; }
 
-        [Value(3, HelpText = "Default argument e.g 'id'.")]
+        [Value(3, HelpText = "Query parameter e.g 'id'.")]
         public string Value2 { get; set; }
 
-        [Value(4, HelpText = "Default argument e.g 'id'.")]
+        [Value(4, HelpText = "Query parameter e.g 'id'.")]
         public string Value3 { get; set; }
 
-        [Value(5, HelpText = "Default argument e.g 'id'.")]
+        [Value(5, HelpText = "Query parameter e.g 'id'.")]
         public string Value4 { get; set; }
 
-        [Value(6, HelpText = "Default argument e.g 'id'.")]
+        [Value(6, HelpText = "Query parameter e.g 'id'.")]
         public string Value5 { get; set; }
 
-        [Value(7, HelpText = "Default argument e.g 'id'.")]
+        [Value(7, HelpText = "Query parameter e.g 'id'.")]
         public string Value6 { get; set; }
 
-        [Value(8, HelpText = "Default argument e.g 'id'.")]
+        [Value(8, HelpText = "Query parameter e.g 'id'.")]
         public string Value7 { get; set; }
 
-        [Value(9, HelpText = "Default argument e.g 'id'.")]
+        [Value(9, HelpText = "Query parameter e.g 'id'.")]
         public string Value8 { get; set; }
 
-        [Value(10, HelpText = "Default argument e.g 'id'.")]
+        [Value(10, HelpText = "Query parameter e.g 'id'.")]
         public string Value9 { get; set; }
     }
 }

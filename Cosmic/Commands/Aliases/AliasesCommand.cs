189516e705addfb80bf62934bf42266e92d2573c
Roger Creyke <roger.creyke@odds-bods.com>
2019-10-27 01:19:34 +0100

    Created 'aliases' command.

---
+++ Cosmic/Commands/Aliases/AliasesCommand.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace Cosmic.Commands.Aliases
+{
+    public class AliasesCommand : Command<AliasesOptions>
+    {
+        protected override Task<int> ExecuteCommandAsync(AliasesOptions options)
+        {
+            return Task.FromResult(0);
+        }
+    }
+}
---
+++ Cosmic/Commands/Aliases/AliasesOptions.cs
@@ -0,0 +1,9 @@
+﻿using CommandLine;
+
+namespace Cosmic.Commands.Aliases
+{
+    [Verb("aliases", HelpText = "List query aliases for common functions.")]
+    public class AliasesOptions
+    {
+    }
+}
